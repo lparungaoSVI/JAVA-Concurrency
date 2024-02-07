970cc9391fee58a6969128bdfc26ff43324dfad8
lparungaoSVI <lparungao@svi.com.ph>
2024-02-07 13:23:37 +0800

    initial

---
+++ AdvancedJavaExercise/src/com/svi/advancedjavatraining/Main.java
@@ -0,0 +1,29 @@
+package com.svi.advancedjavatraining;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		
+		// TODO WORK AREA
+		
+		
+		// TODO END WORK AREA
+		long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
+		long millis = TimeUnit.NANOSECONDS.toMillis(totalTime);
+
+		long seconds = TimeUnit.NANOSECONDS.toSeconds(totalTime);
+
+		long minutes = TimeUnit.NANOSECONDS.toMinutes(totalTime);
+
+		long hours = TimeUnit.NANOSECONDS.toHours(totalTime);
+
+		String time = String.format("%d:%02d:%02d.%03d", hours, minutes, seconds, millis);
+		System.out.println(time);
+
+	}
+
+}
